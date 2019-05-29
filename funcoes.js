19376caea1f0ac640905a75c559d0d5bdf3fcb33
MatheusW <matheuswilgengoncalves@gmail.com>
2019-05-28 21:02:59 -0300

    second commit

--- funcoes.js
+++ funcoes.js
@@ -1,26 +1,66 @@
 
 
-let listaEventos = [{"id":2,"nome":"Evento 02","data":"2019-05-27","created_at":"2019-05-28T00:44:12.048Z","updated_at":"2019-05-28T00:44:12.048Z"},{"id":1,"nome":"Evento 01","data":"2019-05-17","created_at":"2019-05-28T00:43:58.684Z","updated_at":"2019-05-28T00:44:39.774Z"},{"id":3,"nome":"Evento 03","data":"2019-06-06","created_at":"2019-05-28T00:44:52.568Z","updated_at":"2019-05-28T00:44:52.568Z"},{"id":4,"nome":"Evento 04","data":"2019-06-08","created_at":"2019-05-28T00:44:59.654Z","updated_at":"2019-05-28T00:44:59.654Z"},{"id":5,"nome":"Evento 05","data":"2019-06-18","created_at":"2019-05-28T00:45:05.587Z","updated_at":"2019-05-28T00:45:05.587Z"}];
+let eventos = [
 
-
-
-
+];
 
 function filtrar() {
     const data = document.getElementById('data').value;
     console.log(data);
-    
-    
+
+    let eventosFiltrados = [];
+
+    eventos.forEach(evento =>{
+        if(evento.data === data){
+            eventosFiltrados.push(evento);
+        }
+    })
+
     let listaEventosFiltrada = document.getElementById("listaEventosFiltrada");
-    console.log(listaEventosFiltrada);
+    listaEventosFiltrada.innerHTML = '';
 
-    let li = document.createElement("li");
-    li.innerHTML = data;
-    listaEventosFiltrada.appendChild(li);
+    eventosFiltrados.forEach(evento=>{
+        let listaEventosFiltrada = document.getElementById("listaEventosFiltrada");
+        let li = document.createElement("li");
+        li.innerHTML = evento.nome + ' ' + evento.data;
+        listaEventosFiltrada.appendChild(li);
+    })
+}
+console.log(listaEventos);
 
 
-
+let atualizar=()=>{
+    eventos.forEach(evento => {
+        let listaEventos = document.getElementById("listaEventos");
+        let li = document.createElement("li");
+        li.innerHTML = evento.nome + ' ' + evento.data;
+        listaEventos.appendChild(li)
+    })  
 }
 
-let listaEventos = document.getElementById("listaEventos");
-console.log(listaEventos);
+
+
+
+const url = 'http:192.168.25.160:3000/eventos';
+    fetch(url,{
+        method:'GET',
+        headers:{
+            'Authorization':'aaa',
+            'Content-Type' : 'application/json'
+
+
+        }
+    })
+   .then(response=>{
+       return response.json();
+   })
+   .then(res=>{
+       eventos = res;
+       atualizar();
+   }) 
+   .catch(error=>{
+        console.log(error)
+
+   })
+
+    
